8a9650fc0b7c3182cbf723ca7864b9bb672ea172
unknown <miguelestradam36@gmail.com>
2026-07-03 19:42:02 -0600

    Saving changes so far

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ const API_URL = "https://ghibliapi.vercel.app/films";
 
 const moviesContainer = document.getElementById("movies");
 
-const moviesPerPage = 3;
+const moviesPerPage = 6;
 
 let currentPage = 1;
 let movies = [];
@@ -59,6 +59,10 @@ function displayMovies() {
 
                     <h6><strong>Original Title: </strong>${movie.original_title}</h6>
 
+                    <hr>
+
+                    <button onclick="goToDetails('${movie.id}')" class="btn btn-success">See details</button>
+
                 </div>
 
             </div>
@@ -103,6 +107,10 @@ function goToPage(page) {
         behavior: "smooth"
     });
 
+}
+
+function goToDetails(id) {
+    window.location.href = `movie.html?id=${id}`;
 }
 
 getMovies();
---
+++ assets/js/movie.js
@@ -0,0 +1,79 @@
+const API_BASE = "https://ghibliapi.vercel.app/films";
+
+const container = document.getElementById("movie-details");
+
+// Get ID from URL
+const params = new URLSearchParams(window.location.search);
+const movieId = params.get("id");
+
+async function getMovieDetails() {
+
+    try {
+
+        const response = await fetch(`${API_BASE}/${movieId}`);
+
+        if (!response.ok) {
+            throw new Error("Movie not found");
+        }
+
+        const movie = await response.json();
+
+        renderMovie(movie);
+
+    } catch (error) {
+
+        container.innerHTML = `
+            <div class="alert alert-danger text-center">
+                ${error.message}
+            </div>
+        `;
+
+    }
+}
+
+async function renderMovie(movie) {
+
+    container.innerHTML = `
+        <div class="card shadow">
+
+            <img src="${movie.movie_banner}"
+                 class="card-img-top"
+                 alt="${movie.title}">
+
+            <div class="card-body">
+
+                <h1>${movie.title}</h1>
+                <h4>${movie.original_title}</h4>
+                <p><em>${movie.original_title_romanised}</em></p>
+
+                <hr>
+
+                <p><strong>Director:</strong> ${movie.director}</p>
+                <p><strong>Producer:</strong> ${movie.producer}</p>
+                <p><strong>Release:</strong> ${movie.release_date}</p>
+                <p><strong>Running Time:</strong> ${movie.running_time} min</p>
+                <p><strong>Score:</strong> ${movie.rt_score}</p>
+
+                <hr>
+
+                <p>${movie.description}</p>
+
+                <hr>
+
+                <a href="index.html" class="btn btn-success">
+                    ← Back to Explorer
+                </a>
+
+                <a href="${movie.url}"
+                   target="_blank"
+                   class="btn btn-outline-secondary">
+                    API Endpoint
+                </a>
+
+            </div>
+
+        </div>
+    `;
+}
+
+getMovieDetails();
